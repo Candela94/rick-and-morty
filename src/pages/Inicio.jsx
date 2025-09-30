575bfb6b05387f23a413abbd1691b3a66f035438
Candela94 <candelafsg@alumnos.cei.es>
2025-09-30 11:59:58 +0200

    imgs

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -10,13 +10,13 @@ const Inicio = () => {
 
 
             <div className="Inicio">
-                <img className="Inicio-img" src="../public/img/puerta.png" alt="inicio" />
-                <img className="Titulo" src="../public/img/rick-morty.png" alt="" />
+                <img className="Inicio-img" src="/img/puerta.png" alt="inicio" />
+                <img className="Titulo" src="/img/rick-morty.png" alt="" />
                 <div className="Intro-button"><a href="./personajes">Start</a></div>
 
                 <div className="Meeseeks">
                     <img
-                        className="Inicio-img2" src="../public/img/meeseeks.png" alt="inicio" />
+                        className="Inicio-img2" src="/img/meeseeks.png" alt="inicio" />
                     <div className="Circle"></div>
 
                 </div>
